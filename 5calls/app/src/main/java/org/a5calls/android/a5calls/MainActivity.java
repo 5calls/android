3c7d77543ae0da71c61f792dd41b88a6556ee376
Katie Dektar <katie.dektar@gmail.com>
2017-01-27 22:35:32 -0800

    Skeleton app from Android Studio and .gitignores

---
+++ 5calls/app/src/main/java/org/a5calls/android/a5calls/MainActivity.java
@@ -0,0 +1,13 @@
+package org.a5calls.android.a5calls;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
