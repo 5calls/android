ab1458444f1d5256f6a491278ec036129567f556
Katie Dektar <katie.dektar@gmail.com>
2017-01-28 11:04:40 -0800

    Add more to IssueActivity including linkable phone number

--- 5calls/app/src/main/java/org/a5calls/android/a5calls/IssueActivity.java
+++ 5calls/app/src/main/java/org/a5calls/android/a5calls/IssueActivity.java
@@ -3,8 +3,12 @@ package org.a5calls.android.a5calls;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.util.Linkify;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 /**
  * More details about an isssue.
@@ -31,6 +35,19 @@ public class IssueActivity extends AppCompatActivity {
 
         ((TextView) findViewById(R.id.issue_name)).setText(issue.name);
         ((TextView) findViewById(R.id.issue_description)).setText(issue.reason);
+
+        if (issue.contacts != null && issue.contacts.length > 0) {
+            // TODO: Switch between multiple contacts. Remember contact state, show the one
+            // not yet called by this user on this issue.
+            ((TextView) findViewById(R.id.contact_name)).setText(issue.contacts[0].name);
+            Glide.with(getApplicationContext())
+                    .load(issue.contacts[0].photoURL)
+                    .into((ImageView) findViewById(R.id.rep_image));
+
+            TextView phoneText = (TextView) findViewById(R.id.phone_number);
+            phoneText.setText(issue.contacts[0].phone);
+            Linkify.addLinks(phoneText, Linkify.PHONE_NUMBERS);
+        }
 
         // TODO: Add all the reset of the info, including a call button.
     }
