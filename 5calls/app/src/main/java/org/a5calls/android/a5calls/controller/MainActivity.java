f7f2e9f712a61374a35a5e1c1722aea8dea1427f
Katie Dektar <katie.dektar@gmail.com>
2017-02-03 16:45:33 -0800

    Minor UI updates, fixes #28; fixes #27

--- 5calls/app/src/main/java/org/a5calls/android/a5calls/controller/MainActivity.java
+++ 5calls/app/src/main/java/org/a5calls/android/a5calls/controller/MainActivity.java
@@ -90,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
         registerApiListener();
 
+        swipeContainer.setColorSchemeResources(R.color.colorPrimary);
         swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override public void onRefresh() {
                 refreshIssues();
