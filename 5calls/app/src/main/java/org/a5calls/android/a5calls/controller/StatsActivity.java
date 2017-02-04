e00f4d954560de14a28c3f6f90f718376db43c2e
Katie Dektar <katie.dektar@gmail.com>
2017-02-04 07:55:32 -0800

    Adds user stats page

---
+++ 5calls/app/src/main/java/org/a5calls/android/a5calls/controller/StatsActivity.java
@@ -0,0 +1,124 @@
+package org.a5calls.android.a5calls.controller;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.util.Pair;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.a5calls.android.a5calls.AppSingleton;
+import org.a5calls.android.a5calls.R;
+import org.a5calls.android.a5calls.model.DatabaseHelper;
+import org.a5calls.android.a5calls.model.Issue;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Tell the user how great they are!
+ */
+// TODO: Add sharing
+public class StatsActivity extends AppCompatActivity {
+    private static final String TAG = "StatsActivity";
+
+    @BindView(R.id.no_calls_message) TextView noCallsMessage;
+    @BindView(R.id.stats_holder) LinearLayout statsHolder;
+    @BindView(R.id.your_call_count) TextView callCountHeader;
+    @BindView(R.id.stats_contacted) TextView statsContacted;
+    @BindView(R.id.stats_vm) TextView statsVoicemail;
+    @BindView(R.id.stats_unavailable) TextView statsUnavailable;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_stats);
+        ButterKnife.bind(this);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        DatabaseHelper db = AppSingleton.getInstance(this).getDatabaseHelper();
+        int count = db.getCallsCount();
+        if (count == 0) {
+            // Show a "no impact yet!" message.
+            noCallsMessage.setVisibility(View.VISIBLE);
+            statsHolder.setVisibility(View.GONE);
+            return;
+        }
+        noCallsMessage.setVisibility(View.GONE);
+        statsHolder.setVisibility(View.VISIBLE);
+        callCountHeader.setText(count == 1 ?
+                getResources().getString(R.string.your_call_count_one) :
+                String.format(getResources().getString(R.string.your_call_count), count));
+
+        int contactCount = db.getCallsCountForType(IssueActivity.CONTACTED);
+        int vmCount = db.getCallsCountForType(IssueActivity.VOICEMAIL);
+        int unavailableCount = db.getCallsCountForType(IssueActivity.UNAVAILABLE);
+        statsContacted.setText(contactCount == 1 ?
+                getResources().getString(R.string.impact_contact_one) :
+                String.format(getResources().getString(R.string.impact_contact), contactCount));
+        statsVoicemail.setText(vmCount == 1 ? getResources().getString(R.string.impact_vm_one) :
+                String.format(getResources().getString(R.string.impact_vm), vmCount));
+        statsUnavailable.setText(unavailableCount == 1 ?
+                getResources().getString(R.string.impact_unavailable_one) :
+                String.format(getResources().getString(R.string.impact_unavailable),
+                        unavailableCount));
+
+        // There's probably not that many contacts because mostly the user just calls their own
+        // reps. However, it'd be good to move this to a RecyclerView or ListView with an adapter
+        // in the future.
+        // TODO optimize this list creation.
+        List<Pair<String, Integer>> contactStats = db.getCallCountsByContact();
+        LayoutInflater inflater = LayoutInflater.from(this);
+        String callFormatString = getResources().getString(R.string.contact_call_stat);
+        String callFormatStringOne = getResources().getString(R.string.contact_call_stat_one);
+        for (int i = 0; i < contactStats.size(); i++) {
+            String name = db.getContactName(contactStats.get(i).first);
+            if (TextUtils.isEmpty(name)) {
+                name = getResources().getString(R.string.unknown_contact);
+            }
+            TextView contactStat = (TextView) inflater.inflate(R.layout.contact_stat_textview,
+                    null);
+            statsHolder.addView(contactStat);
+            contactStat.setText(contactStats.get(i).second == 1 ?
+                    String.format(callFormatStringOne, name) :
+                    String.format(callFormatString, contactStats.get(i).second, name));
+        }
+
+        /*
+        // Listing issues called doesn't seem so useful since most people will make 1-3 calls per
+        // issue to the same set of contacts.
+        // Can add this in later if there is need.
+        List<Pair<String, Integer>> issueStats = db.getCallCountsByIssue();
+        for (int i = 0; i < issueStats.size(); i++) {
+            String name = db.getIssueName(issueStats.get(i).first);
+            if (TextUtils.isEmpty(name)) {
+                name = getResources().getString(R.string.unknown_issue);
+            }
+            Log.d(TAG, name + " had " + issueStats.get(i).second + " calls");
+        }
+        */
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- 5calls/app/src/main/java/org/a5calls/android/a5calls/model/DatabaseHelper.java
+++ 5calls/app/src/main/java/org/a5calls/android/a5calls/model/DatabaseHelper.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v4.util.Pair;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -18,8 +19,11 @@ import java.util.List;
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String TAG = "DatabaseHelper";
 
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
     private static final String CALLS_TABLE_NAME = "UserCallsDatabase";
+    private static final String ISSUES_TABLE_NAME = "UserIssuesTable";
+    private static final String CONTACTS_TABLE_NAME = "UserContactsTable";
+
 
     private static class CallsColumns {
         public static String TIMESTAMP = "timestamp";
@@ -35,6 +39,23 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                     CallsColumns.ISSUE_ID + " STRING, " + CallsColumns.LOCATION + " STRING, " +
                     CallsColumns.RESULT + " STRING);";
 
+    private static class IssuesColumns {
+        public static String ISSUE_ID = "issueid";
+        public static String ISSUE_NAME = "issuename";
+    }
+
+    private static final String ISSUES_TABLE_CREATE =
+            "CREATE TABLE " + ISSUES_TABLE_NAME + " (" + IssuesColumns.ISSUE_ID + " STRING, " +
+                    IssuesColumns.ISSUE_NAME + " STRING);";
+
+    public static class ContactColumns {
+        public static String CONTACT_ID = "contactid";
+        public static String CONTACT_NAME = "contactname";
+    }
+
+    private static final String CONTACTS_TABLE_CREATE =
+            "CREATE TABLE " + CONTACTS_TABLE_NAME + " (" + ContactColumns.CONTACT_ID + " STRING, " +
+                    ContactColumns.CONTACT_NAME + " STRING);";
 
     public DatabaseHelper(Context context) {
         super(context, CALLS_TABLE_NAME, null, DATABASE_VERSION);
@@ -43,21 +64,29 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CALLS_TABLE_CREATE);
+        db.execSQL(ISSUES_TABLE_CREATE);
+        db.execSQL(CONTACTS_TABLE_CREATE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        if (newVersion >= 2) {
+            db.execSQL(ISSUES_TABLE_CREATE);
+            db.execSQL(CONTACTS_TABLE_CREATE);
+        }
     }
 
     /**
      * Adds a successful call to the user's local database
-     * @param issueId
-     * @param contactId
-     * @param location
-     * @param result
      */
-    public void addCall(String issueId, String contactId, String result, String location) {
+    public void addCall(String issueId, String issueName, String contactId, String contactName,
+                        String result, String location) {
+        addCall(issueId, contactId, result, location);
+        addIssue(issueId, issueName);
+        addContact(contactId, contactName);
+    }
+
+    private void addCall(String issueId, String contactId, String result, String location) {
         ContentValues values = new ContentValues();
         values.put(CallsColumns.TIMESTAMP, System.currentTimeMillis());
         values.put(CallsColumns.CONTACT_ID, contactId);
@@ -65,6 +94,44 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         values.put(CallsColumns.LOCATION, location);
         values.put(CallsColumns.RESULT, result);
         getWritableDatabase().insert(CALLS_TABLE_NAME, null, values);
+    }
+
+    public void addIssue(String issueId, String issueName) {
+        ContentValues values = new ContentValues();
+        values.put(IssuesColumns.ISSUE_ID, issueId);
+        values.put(IssuesColumns.ISSUE_NAME, issueName);
+        // Insert it into the database if it isn't there yet.
+        getWritableDatabase().insertWithOnConflict(ISSUES_TABLE_NAME, null, values,
+                SQLiteDatabase.CONFLICT_IGNORE);
+    }
+
+    public void addContact(String contactId, String contactName) {
+        ContentValues values = new ContentValues();
+        values.put(ContactColumns.CONTACT_ID, contactId);
+        values.put(ContactColumns.CONTACT_NAME, contactName);
+        // Insert it into the database if it isn't there yet.
+        getWritableDatabase().insertWithOnConflict(CONTACTS_TABLE_NAME, null, values,
+                SQLiteDatabase.CONFLICT_IGNORE);
+    }
+
+    public String getIssueName(String issueId) {
+        Cursor c = getReadableDatabase().rawQuery("SELECT " + IssuesColumns.ISSUE_NAME + " FROM " +
+                ISSUES_TABLE_NAME + " WHERE " + IssuesColumns.ISSUE_ID + " = \"" + issueId + "\"",
+                null);
+        if (c.moveToNext()) {
+            return c.getString(0);
+        }
+        return "";
+    }
+
+    public String getContactName(String contactId) {
+        Cursor c = getReadableDatabase().rawQuery("SELECT " + ContactColumns.CONTACT_NAME +
+                " FROM " + CONTACTS_TABLE_NAME + " WHERE " + ContactColumns.CONTACT_ID + " = \"" +
+                contactId + "\"", null);
+        if (c.moveToNext()) {
+            return c.getString(0);
+        }
+        return "";
     }
 
     /**
@@ -136,5 +203,49 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         int count = c.getCount();
         c.close();
         return count;
+    }
+
+    /**
+     * Gets the total number of unavailable calls this user has made
+     */
+    public int getCallsCountForType(String type) {
+        Cursor c = getReadableDatabase().rawQuery(
+                "SELECT " + CallsColumns.TIMESTAMP + " FROM " + CALLS_TABLE_NAME + " WHERE " +
+                CallsColumns.RESULT + " = \"" + type + "\"", null);
+        int count = c.getCount();
+        c.close();
+        return count;
+    }
+
+    /**
+     * Gets a list of pairs of the contacts called and call count for that contact.
+     */
+    public List<Pair<String, Integer>> getCallCountsByContact() {
+        Cursor c = getReadableDatabase().rawQuery(
+                "SELECT " + CallsColumns.CONTACT_ID + ", COUNT(*) as COUNT FROM " +
+                        CALLS_TABLE_NAME + " GROUP BY " + CallsColumns.CONTACT_ID +
+                        " ORDER BY count desc", null);
+        List<Pair<String, Integer>> result = new ArrayList<>();
+        while (c.moveToNext()) {
+            Pair<String, Integer> next = new Pair(c.getString(0), c.getInt(1));
+            result.add(next);
+        }
+        return result;
+    }
+
+    /**
+     * Gets a list of pairs of the issues called and call count for that issue.
+     */
+    public List<Pair<String, Integer>> getCallCountsByIssue() {
+        Cursor c = getReadableDatabase().rawQuery(
+                "SELECT " + CallsColumns.ISSUE_ID + ", COUNT(*) as COUNT FROM " +
+                        CALLS_TABLE_NAME + " GROUP BY " + CallsColumns.ISSUE_ID +
+                        " ORDER BY count desc", null);
+        List<Pair<String, Integer>> result = new ArrayList<>();
+        while (c.moveToNext()) {
+            Pair<String, Integer> next = new Pair(c.getString(0), c.getInt(1));
+            result.add(next);
+        }
+        return result;
     }
 }
