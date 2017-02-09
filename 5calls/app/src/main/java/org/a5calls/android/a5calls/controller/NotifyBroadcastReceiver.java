bd61ffe40997109d7fea679ff9fc02817d8873e0
Katie Dektar <katie.dektar@gmail.com>
2017-02-08 19:19:14 -0800

    notification icon color

--- 5calls/app/src/main/java/org/a5calls/android/a5calls/controller/NotifyBroadcastReceiver.java
+++ 5calls/app/src/main/java/org/a5calls/android/a5calls/controller/NotifyBroadcastReceiver.java
@@ -7,6 +7,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 import org.a5calls.android.a5calls.AppSingleton;
@@ -29,18 +30,22 @@ public class NotifyBroadcastReceiver extends BroadcastReceiver {
         Intent resultIntent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context,
                 MainActivity.NOTIFICATION_REQUEST, resultIntent, PendingIntent.FLAG_CANCEL_CURRENT);
-        Notification builder = new Notification.Builder(context)
+        Notification.Builder builder = new Notification.Builder(context)
                 .setContentTitle(context.getResources().getString(R.string.notification_title))
                 .setContentText(context.getResources().getString(R.string.notification_text))
                 .setContentIntent(pendingIntent) // Launch main activity
                 .setAutoCancel(true) // Goes away when clicked
-                .setSmallIcon(R.drawable.app_icon_bw)
-                .build();
+                .setSmallIcon(R.drawable.app_icon_bw);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder.setColor(context.getResources().getColor(R.color.colorPrimary));
+        }
+        Notification notification = builder.build();
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(NOTIFICATION_ID, builder);
-        // TODO: Add some intent when this is clicked
+        notificationManager.notify(NOTIFICATION_ID, notification);
         // TODO: Add a notification settings option too which goes direct to Settings
-        // TODO: Do not notify if the app is in the foreground (how can we check?)
+        // TODO: Ask UX about the icon
+        // TODO: add option on tutorial to turn off daily reminders
+        // TODO: add an alert for upgrading users to turn off daily reminders
     }
 }
