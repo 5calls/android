2f459b95b2b69019918b39670b076734d644e546
Katie Dektar <katie.dektar@gmail.com>
2017-02-12 10:04:56 -0800

    Update tutorial to use ViewPager and cover reminders

--- 5calls/app/src/main/java/org/a5calls/android/a5calls/controller/TutorialActivity.java
+++ 5calls/app/src/main/java/org/a5calls/android/a5calls/controller/TutorialActivity.java
@@ -5,9 +5,15 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -34,13 +40,7 @@ import butterknife.ButterKnife;
 public class TutorialActivity extends AppCompatActivity {
     private static final String TAG = "TutorialActivity";
 
-    private final AccountManager accountManager = AccountManager.Instance;
-    private FiveCallsApi.RequestStatusListener mStatusListener;
-
-    @BindView(R.id.get_started_btn) Button getStartedButton;
-    @BindView(R.id.calls_to_date) TextView callsToDate;
-    //@BindView(R.id.view_pager) ViewPager viewPager; // TODO use a ViewPager for multiscreens.
-
+    @BindView(R.id.view_pager) ViewPager viewPager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,73 +49,199 @@ public class TutorialActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tutorial);
         ButterKnife.bind(this);
 
-        getStartedButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Return to the main activity
-                accountManager.setTutorialSeen(TutorialActivity.this, true);
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        // TODO: Re-use this listener between AboutActivity and here, since it's really the same.
-        mStatusListener = new FiveCallsApi.RequestStatusListener() {
-            @Override
-            public void onRequestError() {
-                Snackbar.make(callsToDate,
-                        getResources().getString(R.string.request_error),
-                        Snackbar.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onJsonError() {
-                Snackbar.make(callsToDate,
-                        getResources().getString(R.string.json_error),
-                        Snackbar.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onIssuesReceived(String locationName, List<Issue> issues) {
-                // unused
-            }
-
-            @Override
-            public void onCallCount(int count) {
-                callsToDate.setText(String.format(
-                        getResources().getString(R.string.calls_to_date),
-                        NumberFormat.getNumberInstance(Locale.US).format(count)));
-            }
-
-            @Override
-            public void onCallReported() {
-                // unused
-            }
-        };
-        FiveCallsApi controller = AppSingleton.getInstance(getApplicationContext())
-                .getJsonController();
-        controller.registerStatusListener(mStatusListener);
-        controller.getCallCount();
+        viewPager.setAdapter(new TutorialPagerAdapter(getSupportFragmentManager()));
     }
 
     @Override
-    protected void onDestroy() {
-        AppSingleton.getInstance(getApplicationContext()).getJsonController()
-                .unregisterStatusListener(mStatusListener);
-        super.onDestroy();
+    public void onBackPressed() {
+        if (viewPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+        }
+    }
+
+    public void onNextPressed() {
+        viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         // We allow Analytics opt-out.
-        if (accountManager.allowAnalytics(this)) {
+        if (AccountManager.Instance.allowAnalytics(this)) {
             // Obtain the shared Tracker instance.
             FiveCallsApplication application = (FiveCallsApplication) getApplication();
             Tracker tracker = application.getDefaultTracker();
             tracker.setScreenName(TAG);
             tracker.send(new HitBuilders.ScreenViewBuilder().build());
+        }
+    }
+
+    private class TutorialPagerAdapter extends FragmentPagerAdapter {
+
+        public TutorialPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0) {
+                return FirstTutorialPageFragment.newInstance();
+            } else if (position == 1) {
+                return SecondTutorialPageFragment.newInstance();
+            } else if (position == 2) {
+                return ThirdTutorialPageFragment.newInstance();
+            }
+            return null;
+        }
+    }
+
+    public static class FirstTutorialPageFragment extends Fragment {
+        public static FirstTutorialPageFragment newInstance() {
+            FirstTutorialPageFragment fragment = new FirstTutorialPageFragment();
+            return fragment;
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                                 @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.tutorial_item_1, container, false);
+            rootView.findViewById(R.id.btn_next).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ((TutorialActivity) getActivity()).onNextPressed();
+                }
+            });
+            return rootView;
+        }
+    }
+
+    public static class SecondTutorialPageFragment extends Fragment {
+
+        public static SecondTutorialPageFragment newInstance() {
+            SecondTutorialPageFragment fragment = new SecondTutorialPageFragment();
+            return fragment;
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                                 @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.tutorial_item_2, container, false);
+            rootView.findViewById(R.id.btn_back).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getActivity().onBackPressed();
+                }
+            });
+            rootView.findViewById(R.id.btn_next).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ((TutorialActivity) getActivity()).onNextPressed();
+                }
+            });
+            return rootView;
+        }
+    }
+
+    public static class ThirdTutorialPageFragment extends Fragment {
+
+        private FiveCallsApi.RequestStatusListener mStatusListener;
+        private TextView callsToDate;
+
+        public static ThirdTutorialPageFragment newInstance() {
+            ThirdTutorialPageFragment fragment = new ThirdTutorialPageFragment();
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public void onDestroy() {
+            AppSingleton.getInstance(getActivity()).getJsonController()
+                    .unregisterStatusListener(mStatusListener);
+            super.onDestroy();
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                                 @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.tutorial_item_3, container, false);
+            callsToDate = (TextView) rootView.findViewById(R.id.calls_to_date);
+
+            // TODO: Re-use this listener between AboutActivity and here, since it's really the same.
+            mStatusListener = new FiveCallsApi.RequestStatusListener() {
+                @Override
+                public void onRequestError() {
+                    Snackbar.make(callsToDate,
+                            getResources().getString(R.string.request_error),
+                            Snackbar.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onJsonError() {
+                    Snackbar.make(callsToDate,
+                            getResources().getString(R.string.json_error),
+                            Snackbar.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onIssuesReceived(String locationName, List<Issue> issues) {
+                    // unused
+                }
+
+                @Override
+                public void onCallCount(int count) {
+                    callsToDate.setText(String.format(
+                            getResources().getString(R.string.calls_to_date),
+                            NumberFormat.getNumberInstance(Locale.US).format(count)));
+                }
+
+                @Override
+                public void onCallReported() {
+                    // unused
+                }
+            };
+            FiveCallsApi controller = AppSingleton.getInstance(getActivity())
+                    .getJsonController();
+            controller.registerStatusListener(mStatusListener);
+            controller.getCallCount();
+
+            rootView.findViewById(R.id.btn_back).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getActivity().onBackPressed();
+                }
+            });
+            rootView.findViewById(R.id.get_started_btn).setOnClickListener(
+                    new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Set that the user has seen info about reminders.
+                    AccountManager.Instance.setRemindersInfoShown(getActivity(), true);
+
+                    // Return to the main activity
+                    AccountManager.Instance.setTutorialSeen(getActivity(), true);
+                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+                }
+            });
+            return rootView;
         }
     }
 }
