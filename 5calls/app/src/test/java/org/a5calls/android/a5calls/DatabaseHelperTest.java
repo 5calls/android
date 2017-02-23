9b6f36c8ec4d06b89e0b32ad2d303235aaa8343a
Katie Dektar <katie.dektar@gmail.com>
2017-02-22 19:58:25 -0800

    Sanitize contact IDs. Fixes #53

---
+++ 5calls/app/src/test/java/org/a5calls/android/a5calls/DatabaseHelperTest.java
@@ -0,0 +1,22 @@
+package org.a5calls.android.a5calls;
+
+import org.a5calls.android.a5calls.model.DatabaseHelper;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for DatabaseHelper.
+ */
+public class DatabaseHelperTest {
+    @Test
+    public void sanitizesStrings() throws Exception {
+        assertEquals("TX-BetoO''Rourke", DatabaseHelper.sanitizeContactId("TX-BetoO'Rourke"));
+        assertEquals("cats", DatabaseHelper.sanitizeContactId("cats"));
+        assertEquals("cat''s", DatabaseHelper.sanitizeContactId("cat's"));
+        assertEquals("cat''s cats cats''", DatabaseHelper.sanitizeContactId("cat's cats cats'"));
+
+        // Don't sanitize double quotes
+        assertEquals("cat''s", DatabaseHelper.sanitizeContactId("cat''s"));
+    }
+}
