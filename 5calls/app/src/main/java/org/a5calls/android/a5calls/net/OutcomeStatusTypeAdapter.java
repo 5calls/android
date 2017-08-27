f73d3a75d5c5e5555fa9905a97dc37fb1d735826
Bryan Sills <bryan@bryansills.com>
2017-08-26 22:15:18 -0500

    Remove commented code

--- 5calls/app/src/main/java/org/a5calls/android/a5calls/net/OutcomeStatusTypeAdapter.java
+++ 5calls/app/src/main/java/org/a5calls/android/a5calls/net/OutcomeStatusTypeAdapter.java
@@ -9,24 +9,6 @@ import org.a5calls.android.a5calls.model.Outcome;
 import java.io.IOException;
 
 public class OutcomeStatusTypeAdapter extends TypeAdapter<Outcome.Status> {
-//    @Override
-//    public Outcome.Status deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-//        Outcome.Status result = Outcome.Status.UNKNOWN;
-//
-//        for (Outcome.Status status : Outcome.Status.values()) {
-//            if (status.toString().equals(json.getAsString())) {
-//                result = status;
-//            }
-//        }
-//
-//        return result;
-//    }
-//
-//    @Override
-//    public JsonElement serialize(Outcome.Status src, Type typeOfSrc, JsonSerializationContext context) {
-//        return new JsonPrimitive(src.toString());
-//    }
-
     @Override
     public void write(JsonWriter out, Outcome.Status value) throws IOException {
         out.name("status").value(value.toString());
