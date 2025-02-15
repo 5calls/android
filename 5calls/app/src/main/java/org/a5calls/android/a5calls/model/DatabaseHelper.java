7c9447c4d51b985bab9c51f341c9067eda6a5aee
Katie Dektar <dektar@users.noreply.github.com>
2025-02-15 08:19:52 -0800

    Show the user their daily calls, and move total calls to its own row. (#171)
    * First pass at showing calls today vs previous calls
    
    * Clean up code logic, add comments
    
    * Congratulations text for finishing today

--- 5calls/app/src/main/java/org/a5calls/android/a5calls/model/DatabaseHelper.java
+++ 5calls/app/src/main/java/org/a5calls/android/a5calls/model/DatabaseHelper.java
@@ -183,7 +183,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     /**
-     * Gets the calls in the database for a particular issue and list of contacts.
+     * Gets the contacts in the database who were contacted for a particular issue and list of
+     * contacts.
      * @param issueId
      * @param contacts
      * @return A list of the contact IDs contacted for this issue.
@@ -202,6 +203,30 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         List<String> result = new ArrayList<>();
         while (c.moveToNext()) {
             result.add(c.getString(0));
+        }
+        c.close();
+        return result;
+    }
+
+    /**
+     * Gets the total number of calls made for a given issue and list of contacts.
+     * @param issueId
+     * @param contacts
+     * @return total calls
+     */
+    public int getTotalCallsForIssueAndContacts(String issueId, List<Contact> contacts) {
+        String[] contactIdList = new String[contacts.size()];
+        for (int i = 0; i < contacts.size(); i++) {
+            contactIdList[i] = "'" + sanitizeContactId(contacts.get(i).id) + "'";
+        }
+        String contactIds = "(" + TextUtils.join(",", contactIdList) + ")";
+        String query = "SELECT " + CallsColumns.CONTACT_ID + " FROM " +
+                CALLS_TABLE_NAME + " WHERE " + CallsColumns.ISSUE_ID + " = ? AND " +
+                CallsColumns.CONTACT_ID + " IN " + contactIds;
+        Cursor c = getReadableDatabase().rawQuery(query, new String[] {issueId});
+        int result = 0;
+        while (c.moveToNext()) {
+            result++;
         }
         c.close();
         return result;
