41fb46ede952ea383b7bbd4f1024bda92a19ce84
Katie Dektar <dektar@users.noreply.github.com>
2025-09-09 07:49:16 -0700

    Show placeholder rep lines for vacant seats, fixes #247 (#262)
    * Show placeholder rep lines for vacant seats
    
    Creates placeholder reps in the list of reps in IssueActivity when we determine there is a vacancy. No change to the main issues list or contacts count. Placeholder is not clickable and is styled slightly differently to make it look inactive.
    
    Test: Manual with missing house reps, with and without state issues. Ensured issue done pane is only shown when all actual reps are called. Ensured correct handling of house-only issue with no house member. Checked addresses in DC to ensure house-only districts handled OK. Checked addresses with and without house reps in Texas to ensure state-only issues are still handled correctly.
    
    * Spanish translations
    
    * Add all contact area constants

--- 5calls/app/src/main/java/org/a5calls/android/a5calls/model/Contact.java
+++ 5calls/app/src/main/java/org/a5calls/android/a5calls/model/Contact.java
@@ -13,6 +13,14 @@ import org.a5calls.android.a5calls.R;
  *
  */
 public class Contact implements Parcelable {
+    public static final String AREA_HOUSE = "US House";
+    public static final String AREA_SENATE = "US Senate";
+    public static final String AREA_STATE_LOWER = "StateLower";
+    public static final String AREA_STATE_UPPER = "StateUpper";
+    public static final String AREA_GOVERNOR = "Governor";
+    public static final String AREA_ATTORNEY_GENERAL = "AttorneyGeneral";
+    public static final String AREA_SECRETARY_OF_STATE = "SecretaryOfState";
+
     public String id;
     public String name;
     public String phone;
@@ -23,6 +31,10 @@ public class Contact implements Parcelable {
     public String area;
     public String district;
     public FieldOffice[] field_offices;
+    // This defaults to false when populated with JSON that doesn't have
+    // the equivalent key. It is just used within the app to denote a placeholder
+    // rep, e.g. used for a vacant seat.
+    public boolean isPlaceholder;
 
     protected Contact(Parcel in) {
         id = in.readString();
@@ -35,6 +47,19 @@ public class Contact implements Parcelable {
         area = in.readString();
         district = in.readString();
         field_offices = in.createTypedArray(FieldOffice.CREATOR);
+        isPlaceholder = in.readInt() == 1;
+    }
+
+    protected Contact(String id, String name, String reason, String area, boolean isPlaceholder) {
+        this.id = id;
+        this.name = name;
+        this.reason = reason;
+        this.area = area;
+        this.isPlaceholder = isPlaceholder;
+    }
+
+    public static Contact createPlaceholder(String id, String name, String reason, String area) {
+        return new Contact(id, name, reason, area, /* isPlaceholder= */ true);
     }
 
     public static final Creator<Contact> CREATOR = new Creator<Contact>() {
@@ -66,6 +91,7 @@ public class Contact implements Parcelable {
         dest.writeString(area);
         dest.writeString(district);
         dest.writeTypedArray(field_offices, PARCELABLE_WRITE_RETURN_VALUE);
+        dest.writeInt(isPlaceholder ? 1 : 0);
     }
 
     public String getDescription(Resources res) {
